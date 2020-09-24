f193a2d8774d6f551ba3c06064796ec329eae94a
NathanChildress <r.nathanchildress@gmail.com>
2020-09-23 20:09:02 -0700

    added our basic models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Boardgame(models.Model):
+    name = models.CharField(max_length=50)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={ 'Boardgame_id': self.id })
+
+class Reviews(models.Model):
+    boardgame = models.ForeignKey(Boardgame, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
